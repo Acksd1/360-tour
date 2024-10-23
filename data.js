a284883f6dcd2f498b38a342d853a915a1c3e460
Acksd1 <wxudong14@gmail.com>
2024-10-23 15:43:28 +0800

    First commit
    The first commit of my tour

---
+++ data.js
@@ -0,0 +1,146 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.7173971520031781,
+        "pitch": 0,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3250498149583283,
+          "pitch": -0.1863405015902142,
+          "rotation": 0,
+          "target": "2-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6270466075223666,
+          "pitch": 0.002198149343325184,
+          "title": "Welcome to engineering",
+          "text": "This is the engineering sign"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1135226442324537,
+          "pitch": 0.0015544004946583811,
+          "rotation": 0,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.847278450470494,
+          "pitch": -0.2074176558007128,
+          "title": "Cafe",
+          "text": "Can buy coffee and food here"
+        }
+      ]
+    },
+    {
+      "id": "2-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07229039300526274,
+          "pitch": -0.06229115781584582,
+          "rotation": 0,
+          "target": "1-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4338483951120686,
+          "pitch": -0.36450421262980015,
+          "title": "Statue",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Faculty of engineering",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
